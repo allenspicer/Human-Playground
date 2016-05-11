38e1e385cfdce6dd886a0adc7f6760848dfed606
Allen Spicer <allenspicer@gmail.com>
2016-05-10 20:26:04 -0400

    Complete

--- Human.playground/Contents.swift
+++ Human.playground/Contents.swift
@@ -2,8 +2,6 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
 class BodyPart{
     
     
@@ -12,23 +10,74 @@ class BodyPart{
 
 //1
 class finger: BodyPart {}
+func flick() {}
+func touch() {}
+let nail:Bool
+let index:Int
+
+
 //2
 class hand: BodyPart{}
+func hold(){}
+func shake(){}
+let side:Bool
+let size:Int
+
 //3
 class arm: BodyPart{}
+func extend(){}
+func rotate(){}
+let length:Int
+let side2:Bool
+
 //4
 class torso: BodyPart {}
+func move(){}
+func expand(){}
+let circumference:Int
+let height:Int
+
 //5
 class head: BodyPart{}
+func nod(){}
+func swivel(){}
+let volume: Int
+let features: String
+
 //6
 class waist: BodyPart {}
+func bend(){}
+func twist(){}
+let width:Int
+let circumference2:Int
+
 //7
 class leg: BodyPart {}
+func kick(){}
+func brace(){}
+let length2:Int
+let color:UIColor
+
 //8
 class foot: BodyPart {}
+func flex(){}
+let surfaceArea:Int
+let hair:Bool
+
+
 //9
 class toe: BodyPart{}
+func wiggle(){}
+func curl(){}
+let description:String
+let girth:Int
+
+
 //10
 class face: BodyPart{}
+func scrunch(){}
+func rest(){}
+let eyes:Bool
+let nose:Bool
 
 
