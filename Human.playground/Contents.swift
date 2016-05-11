240b69591cb9661afd2b05e8c4b2a3e5e8ded923
Allen Spicer <allenspicer@gmail.com>
2016-05-10 20:02:33 -0400

    Initial Commit

---
+++ Human.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class BodyPart{
+    
+    
+}
+
+
+//1
+class finger: BodyPart {}
+//2
+class hand: BodyPart{}
+//3
+class arm: BodyPart{}
+//4
+class torso: BodyPart {}
+//5
+class head: BodyPart{}
+//6
+class waist: BodyPart {}
+//7
+class leg: BodyPart {}
+//8
+class foot: BodyPart {}
+//9
+class toe: BodyPart{}
+//10
+class face: BodyPart{}
+
+
